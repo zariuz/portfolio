861fc27b1cf56cedf98c5fe7a2ffefc5ca517b8b
zariuz <nechetov@hotmail.com>
2020-08-28 10:36:03 +0300

    start project

---
+++ src/app/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
+import './App.css';
+
+function App() {
+  return <div>Hello!</div>;
+}
+
+export default App;
